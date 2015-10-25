0f399fc53299da1ea25aac6de261156d6b8045ce
saasbook <saasbook@saasbook.(none)>
2015-10-25 11:39:34 -0300

    My blog create action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,12 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path, :notice => "Successfully created!"
+		else
+			render "new"
+		end
 	end
 	def edit
 	end
