8bb4aa2800bd8828ffd28514fbd7e279275852dd
saasbook <saasbook@saasbook.(none)>
2015-10-25 08:46:15 -0300

    My blog index view created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+	def index
+		@posts=Post.all
+	end
+	def new
+	end
+	def show
+	end
+	def creat
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+end
